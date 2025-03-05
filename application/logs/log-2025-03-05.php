711781cd743f19118f5d637b9ba1521b9dec7a17
raviwebadzguru <php.ravikr84@gmail.com>
2025-03-05 10:46:07 +0530

    log commit

---
+++ application/logs/log-2025-03-05.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-03-05 13:57:46 --> Severity: Warning --> file_get_contents(): php_network_getaddresses: getaddrinfo for update.adzguru.co failed: No such host is known.  D:\xampp\htdocs\punjabi-palace\application\modules\template\views\includes\header.php 19
+ERROR - 2025-03-05 13:57:46 --> Severity: Warning --> file_get_contents(https://update.adzguru.co/punjabi_palace/autoupdate/update_info): Failed to open stream: php_network_getaddresses: getaddrinfo for update.adzguru.co failed: No such host is known.  D:\xampp\htdocs\punjabi-palace\application\modules\template\views\includes\header.php 19
+ERROR - 2025-03-05 14:11:32 --> Severity: Warning --> file_get_contents(): php_network_getaddresses: getaddrinfo for update.adzguru.co failed: No such host is known.  D:\xampp\htdocs\punjabi-palace\application\modules\template\views\includes\header.php 19
+ERROR - 2025-03-05 14:11:32 --> Severity: Warning --> file_get_contents(https://update.adzguru.co/punjabi_palace/autoupdate/update_info): Failed to open stream: php_network_getaddresses: getaddrinfo for update.adzguru.co failed: No such host is known.  D:\xampp\htdocs\punjabi-palace\application\modules\template\views\includes\header.php 19
+ERROR - 2025-03-05 14:11:32 --> Severity: Warning --> Undefined variable $title2 D:\xampp\htdocs\punjabi-palace\application\modules\template\views\includes\header.php 69
+ERROR - 2025-03-05 14:11:32 --> Severity: Warning --> Undefined variable $title2 D:\xampp\htdocs\punjabi-palace\application\modules\template\views\includes\header.php 133
+ERROR - 2025-03-05 05:59:15 --> Severity: error --> Exception: Table 'punjab-palace_db.currency' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 05:59:23 --> Severity: error --> Exception: Table 'punjab-palace_db.currency' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 05:59:33 --> Severity: error --> Exception: Table 'punjab-palace_db.paymentsetup' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 05:59:43 --> Severity: error --> Exception: Table 'punjab-palace_db.paymentsetup' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 05:59:53 --> Severity: error --> Exception: Table 'punjab-palace_db.paymentsetup' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 06:04:54 --> Severity: error --> Exception: Table 'punjab-palace_db.paymentsetup' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 06:05:04 --> Severity: error --> Exception: Table 'punjab-palace_db.paymentsetup' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 06:05:14 --> Severity: error --> Exception: Table 'punjab-palace_db.paymentsetup' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 06:06:15 --> Severity: error --> Exception: Table 'punjab-palace_db.paymentsetup' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
+ERROR - 2025-03-05 06:07:15 --> Severity: error --> Exception: Table 'punjab-palace_db.paymentsetup' doesn't exist D:\xampp\htdocs\punjabi-palace\system\database\drivers\mysqli\mysqli_driver.php 317
