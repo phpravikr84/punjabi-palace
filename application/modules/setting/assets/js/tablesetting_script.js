3a10eaf0635496751470b931e4b8c60f25cc7869
raviwebadzguru <php.ravikr84@gmail.com>
2025-08-12 13:10:36 +0530

    new table images and add rotation and table name in table setting

--- application/modules/setting/assets/js/tablesetting_script.js
+++ application/modules/setting/assets/js/tablesetting_script.js
@@ -1,21 +1,57 @@
- $( function() {
- 	"use strict"; 
-	$( ".draggable" ).draggable({
-		cursor: "move",
-  		stop: function( event, ui ) {
-	   var id=event.target.id;
-	   var style=$("#"+id).attr('style');
-	   var csrf = $('#csrfhashresarvation').val();
-	   var dataString="ids="+id+"&style="+style+"&csrf_test_name="+csrf;
-	    $.ajax({
-		 type: "POST",
-		 url: basicinfo.baseurl+'setting/restauranttable/updatesetting',
-		 data: dataString,
-		 success: function(data) {
-			
-		 } 
-	});
-	   
-	  }});
-  }); 
- 
+$(function () {
+    "use strict";
+
+    $(".draggable").css("position", "relative");
+
+    $(".draggable").each(function () {
+        var $el = $(this);
+
+        // Make only the image inside rotatable
+        $el.find(".rotatable-content").rotatable({
+            handle: $('<div class="ui-rotatable-handle" style="display:none;"></div>'),
+            wheelRotate: false,
+            stop: function () {
+                saveTablePosition($el);
+            }
+        });
+
+        // Make the whole block draggable
+        $el.draggable({
+            cursor: "move",
+            stop: function () {
+                saveTablePosition($el);
+            }
+        });
+
+        // On click — show only this table's handle
+        $el.on("click", function (e) {
+            e.stopPropagation();
+            $(".ui-rotatable-handle").hide();
+            $(this).find(".ui-rotatable-handle").show();
+        });
+
+        // On double-click the handle — hide it
+        $el.on("dblclick", ".ui-rotatable-handle", function (e) {
+            e.stopPropagation();
+            $(this).hide();
+        });
+    });
+
+    // Hide handles when clicking outside
+    $(document).on("click", function () {
+        $(".ui-rotatable-handle").hide();
+    });
+
+    function saveTablePosition($el) {
+        var id = $el.attr('id');
+        var style = $el.attr('style');
+        var csrf = $('#csrfhashresarvation').val();
+        var dataString = "ids=" + id + "&style=" + style + "&csrf_test_name=" + csrf;
+
+        $.ajax({
+            type: "POST",
+            url: basicinfo.baseurl + 'setting/restauranttable/updatesetting',
+            data: dataString
+        });
+    }
+});
